e987a2ea1fd902a9ffd8809d0a0f5c9ee07e7d65
Existent-io <g.tselkovskiy@gmail.com>
2022-07-28 01:03:54 +0300

    small refactoring

--- app/src/main/java/com/existentio/networkapisampleapp/ui/GifDetailedOverviewFragment.kt
+++ app/src/main/java/com/existentio/networkapisampleapp/ui/GifDetailedOverviewFragment.kt
@@ -20,7 +20,6 @@ class GifDetailedOverviewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentGifDetailedOverviewBinding
             .inflate(inflater, container, false)
         return binding.root
